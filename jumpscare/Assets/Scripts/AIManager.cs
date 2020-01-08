3f893f1889484eb8735ec1a65d3dd8155b62f09f
Roumiana Bourkova <48831445+rumywanderung@users.noreply.github.com>
2020-01-08 14:04:22 +0100

    1 enemy following player

---
+++ jumpscare/Assets/Scripts/AIManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class AIManager : MonoBehaviour
+{
+    public GameManager manager; //target
+    Transform target;
+    public NavMeshAgent AI_1; //agent
+    //public float radius = 2F; //juste pour 1 AI
+
+    void Start()
+    {
+        manager = FindObjectOfType<GameManager>();
+        target = manager.player.gameObject.transform;
+    }
+    
+    void Update()
+    {
+        AI_1.SetDestination(target.position);
+    }
+}
---
+++ jumpscare/Assets/Scripts/GameManager.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public GameObject player;
+    public AIManager aimanager;
+
+    void Start()
+    {
+        aimanager = FindObjectOfType<AIManager>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
