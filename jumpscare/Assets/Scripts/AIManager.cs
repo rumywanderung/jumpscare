1468e1023efe1a2eadf54f82fc7194b04022e28a
Roumiana Bourkova <48831445+rumywanderung@users.noreply.github.com>
2020-01-19 14:50:08 +0100

    working on detection

--- jumpscare/Assets/Scripts/AIManager.cs
+++ jumpscare/Assets/Scripts/AIManager.cs
@@ -15,6 +15,7 @@ public class AIManager : MonoBehaviour
 
     private bool inPoint = false;
     private bool toPoint = false;
+    public float distance;
     bool hitDetect;
 
     public Collider enemy1Collider;
@@ -23,34 +24,47 @@ public class AIManager : MonoBehaviour
     {
         manager = FindObjectOfType<GameManager>();
         target = manager.player.gameObject.transform;
+        distance = 10F;
         //enemy1Collider = AI_1.gameObject.GetComponent<Collider>();
     }
     
     void Update()
     {
-        /* if (Physics.Raycast(transform.position, Vector3.forward, out hit, 50F))
-        {
-            Debug.Log(hit.rigidbody.name);
-        }*/
         AI_1.SetDestination(target.position);
+
+        // raycast
+
+        RaycastHit hit;
+        if (Physics.Raycast(AI_1.transform.position, AI_1.transform.TransformDirection(Vector3.forward), out hit, distance))
+        {
+            Debug.DrawRay(AI_1.transform.position, AI_1.transform.TransformDirection(Vector3.forward) * hit.distance, Color.yellow);
+            //Debug.Log("Did Hit");
+            //Add small rays
+        }
+        else
+        {
+            Debug.DrawRay(AI_1.transform.position, AI_1.transform.TransformDirection(Vector3.forward) * 1000, Color.white);
+            //Debug.Log("Did not Hit");
+            //Add huge red rays
+        }
     }
 
-    private void OnDrawGizmos()
+    /*private void OnDrawGizmos()
     {
         RaycastHit hit;
         Gizmos.color = Color.red;
 
-        hitDetect = Physics.BoxCast(enemy1Collider.bounds.center, transform.localScale, transform.forward, out hit, transform.rotation, 30F);
+        hitDetect = Physics.BoxCast(enemy1Collider.bounds.center, transform.localScale, transform.forward, out hit, transform.rotation, distance);
         if (hitDetect)
         {
             Debug.Log("we found!!!");
-            Gizmos.DrawRay(transform.position, transform.forward * 30F);
-            Gizmos.DrawWireCube(transform.position + transform.forward * 30F, transform.localScale);
+            Gizmos.DrawRay(transform.position, transform.forward * distance);
+            Gizmos.DrawWireCube(transform.position + transform.forward * distance, transform.localScale);
         }
         else
         {
             Debug.Log("we didn't find anyone");
         }
             
-    }
+    }*/
 }
